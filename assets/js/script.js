6113dc697209e683bb52b3c6846b8fe13fafa2fb
cami-negrete <camila.negrete@gmail.com>
2024-07-11 15:34:42 -0400

    últimos detalles

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $("a").click(function () {
+      /* para saber que id es que estoy capturando
+      console.log("voy a buscar esto:" + this.hash) */
+      if(this.hash !==""){
+        event.preventDefault();
+  
+        var hash= this.hash;
+  
+        $("html, body").animate({
+          scroolTop : $(hash).offset().top
+        }, 800, function(){
+          window.location.hash = hash;
+        })
+      }
+       
+    });
+  });
